d24de2af1e5eb6f703e4f1ece4ec9248841dc51b
Jianyu <744795167@qq.com>
2024-08-22 11:40:18 +0800

    初步创建登录界面和主界面ui

---
+++ components/rectchoice/rectChoice.js
@@ -0,0 +1,20 @@
+// components/rectchoice/rectChoice.js
+Component({
+  data: {
+
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    backgroundSrc: String,
+    rectName: String
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- pages/home/home.js
+++ pages/home/home.js
@@ -4,22 +4,41 @@ Page({
     username: '',
     password: ''
   },
-  bindInput: function(e) {
-    const key = e.currentTarget.dataset.key;
-    this.setData({
-      [key]: e.detail.value
-    });
-  },
+
+  // //用户名输入处理
+  // bindUserNameInput: function(e) {
+  //   console.log('bindUserNameInput');
+  //   const key = e.currentTarget.dataset.key;
+  //   this.setData({
+  //     username: e.detail.value
+  //   });
+  // },
+
+  // //密码输入处理
+  // bindPasswordInput: function(e) {
+  //   console.log('bindPasswordInput');
+  //   const key = e.currentTarget.dataset.key;
+  //   this.setData({
+  //     password: e.detail.value
+  //   });
+  // },
+
+
   onSubmit: function(e) {
     const { username, password } = this.data;
     
     console.log('用户名：', username);
     console.log('密码：', password);
-    // 模拟登录成功
+
     wx.showToast({
       title: '登录成功',
       icon: 'success',
       duration: 2000
     });
+
+    wx.navigateTo({
+      url:  "/pages/index/index"
+    });
+
   }
 });
